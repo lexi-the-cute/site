3bf7cf3373133b026f9e2be48a552f7f603173cf
Alexis <120563837+alexisart@users.noreply.github.com>
2023-03-17 00:30:40 -0400

    Force SSR Runtime Via Slug

---
+++ app/database/[slug]/page.tsx
@@ -0,0 +1,19 @@
+// @ts-nocheck
+// TODO: Determine Why <Posts/> Causes Type Check Problems
+import { Suspense, lazy } from 'react';
+
+const Posts = lazy(() => import('../../../lib/components/Posts'));
+
+export default function Page({params}) {
+	return (
+		<>
+			<Suspense fallback={<Loading />}>
+				<h1>Hello <Posts/></h1>
+			</Suspense>
+		</>
+	)
+}
+
+function Loading() {
+	return <h2>Loading...</h2>
+}
