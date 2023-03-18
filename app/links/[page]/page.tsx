0c15b79d20dbd1732ec01830b86c654e69ecd1e2
Alexis <120563837+alexisart@users.noreply.github.com>
2023-03-18 01:02:27 -0400

    Fixed Error Handling

--- app/links/[page]/page.tsx
+++ app/links/[page]/page.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import { Suspense, lazy } from 'react'
 import prisma from '../../../lib/prisma'
+import { notFound } from 'next/navigation'
 import Loading from '../../../lib/components/Loading'
 import TablerIcons from '../../../lib/components/TablerIcons'
 
@@ -14,18 +15,29 @@ function getLinks(page) {
 
 export default async function Page({params}) {
 	const page = params.page
-	const links = await getLinks(page)
-	
-	return (
-		<div className="social-links">
-			{/* @ts-expect-error { <Loading /> causes missing property error on build } */}
-			<Suspense fallback={<Loading />}>
-				{links.map((link) => (
-					<div key={String(link.id)} className={`social-link page-${link.page}`}>
-						<Link href={link.url}><TablerIcons icon={link.icon}/>{link.text}</Link>
-					</div>
-				))}
-			</Suspense>
-		</div>
-	)
+	try {
+		const links = await getLinks(page)
+		
+		if (links.length <= 0) {
+			// No Links Found
+			notFound()
+		}
+		
+		return (
+			<div className="social-links">
+				{/* @ts-expect-error { <Loading /> causes missing property error on build } */}
+				<Suspense fallback={<Loading />}>
+					{links.map((link) => (
+						<div key={String(link.id)} className={`social-link page-${link.page}`}>
+							<Link href={link.url}><TablerIcons icon={link.icon}/>{link.text}</Link>
+						</div>
+					))}
+				</Suspense>
+			</div>
+		)
+	} catch(error) {
+		// Database Error (e.g. table does not exist)
+		console.error(`Database Error: '${error}' on Page '/links/${page}'. Try checking if your links table exists...`)
+		notFound()
+	}
 }
